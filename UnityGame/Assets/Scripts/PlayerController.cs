45bb3926414f80c4824fa27b0a01954d2221f63a
paveq12 <paveq12@gmail.com>
2017-03-07 22:36:34 +0100

    Commit 2

--- UnityGame/Assets/Scripts/PlayerController.cs
+++ UnityGame/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Animator anim;
+
 	// Use this for initialization
 	void Start () {
+
+        anim = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(horizontalMove));
 		
 	}
 }
