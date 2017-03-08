56ddaf1aaaa4c465cb88e5f57e95df35afe9f3cb
paveq12 <paveq12@gmail.com>
2017-03-08 23:19:57 +0100

    Commit 3

--- UnityGame/Assets/Scripts/PlayerController.cs
+++ UnityGame/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float heroSpeed;
     Animator anim;
+    bool dirToRight = true;
+    Rigidbody2D rgdBody;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         anim = GetComponent<Animator>();
-		
+        rgdBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         float horizontalMove = Input.GetAxis("Horizontal");
+        rgdBody.velocity = new Vector2(horizontalMove * heroSpeed, rgdBody.velocity.y);
         anim.SetFloat("speed", Mathf.Abs(horizontalMove));
-		
+
+        if (horizontalMove < 0 && dirToRight)
+        {
+            Flip();
+        }
+        if (horizontalMove > 0 && !dirToRight)
+        {
+            Flip();
+        }
 	}
+
+    void Flip ()
+    {
+        dirToRight = !dirToRight;
+        Vector3 heroScale = gameObject.transform.localScale;
+        heroScale.x *= -1;
+        gameObject.transform.localScale = heroScale;
+    }
 }
