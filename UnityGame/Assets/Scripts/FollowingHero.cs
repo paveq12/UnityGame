c055b0ca54c30d016bd5c957555bc963c6d40b01
paveq12 <paveq12@gmail.com>
2017-03-13 22:01:48 +0100

    Commit

---
+++ UnityGame/Assets/Scripts/FollowingHero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingHero : MonoBehaviour {
+
+    public GameObject hero;
+    public float smooth;
+
+    private Vector3 currVelocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Podążanie za bohaterem tylko w osi x
+        Vector3 newCameraPosition = new Vector3(hero.transform.position.x, transform.position.y, transform.position.z);
+        // Dodanie SmoothDamp czyli płynne podążanie kamery
+        transform.position = Vector3.SmoothDamp(transform.position, newCameraPosition, ref currVelocity, smooth);
+		
+	}
+}
---
+++ UnityGame/Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+    public Transform cameraTransform;
+    // Współczynnik paralaxy
+    public float parallaxFactory;
+
+    // Różnice przesunięcia kamery
+    private Vector3 prevCameraPosition;
+    private Vector3 deltaCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+
+        prevCameraPosition = cameraTransform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Różnica między bierzącą pozycją kamery a poprzednią
+        deltaCameraPosition = cameraTransform.position - prevCameraPosition;
+        Vector3 parallaxPosition = new Vector3(transform.position.x + (deltaCameraPosition.x * parallaxFactory), transform.position.y, transform.position.x);
+        transform.position = parallaxPosition;
+        prevCameraPosition = cameraTransform.position;
+
+    }
+}
