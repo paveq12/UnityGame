724be9c372c7e498b18018a490dcace1b77efa06
paveq12 <paveq12@gmail.com>
2017-03-17 20:38:50 +0100

    Commit

---
+++ UnityGame/Assets/Scripts/FailSpikeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailSpikeController : MonoBehaviour {
+
+    public Transform startPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Metoda zostanie wywołana jeśli jakiś obiekt wejdzie w kolizje z kolcami
+	void OnTriggerEnter2D (Collider2D other)
+    {   // Jeżeli ktoś wejdzie w kolce to zostanie przeniesiony do pozycji startowej
+        if (other.gameObject.name == "Player")
+        {
+            other.transform.position = startPoint.position;
+        }
+    }
+}
