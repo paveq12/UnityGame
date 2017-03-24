b1cb70a81105619bb969623d3ff83bf5071524d6
paveq12 <paveq12@gmail.com>
2017-03-24 21:43:24 +0100

    Commit

---
+++ UnityGame/Assets/Scripts/PickUpBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other){ // Jeżeli wejdziemy w obiek bohaterem to się usunie
+        if (other.gameObject.name == "Player"){
+            Destroy(this.gameObject);
+        }
+    }
+}
